dbb6054204ab862e92790a52dbf85fd27fb5d7c9
mzaini30 <muhzaini30@gmail.com>
2016-07-07 00:24:10 +0800

    tambahin run lah

--- blog/js/app.js
+++ blog/js/app.js
@@ -1,21 +1,27 @@
-var blog2 = blog.slice(0)
-for (nama in blog2) {
-    blog2[nama] = blog2[nama].split(", ")
-}
-baru = function(x) {
-    document.writeln("<a href='" + blog2[x][1] + "' class='list-group-item'>" + blog2[x][0] + "</a>")
-}
-
-
+// deklarasi fungsi
 random = function(array) {
     array.sort(function() {
         return Math.random() - .5
     })
 }
-random(blog)
-for (nama in blog) {
-    blog[nama] = blog[nama].split(", ")
+
+// duplikat array
+arraybaru = blog.slice(0)
+arrayacak = blog.slice(0)
+
+// baru
+for (nama in arraybaru) {
+    arraybaru[nama] = arraybaru[nama].split(", ")
+}
+baru = function(x) {
+    document.writeln("<a href='" + arraybaru[x][1] + "' class='list-group-item'>" + arraybaru[x][0] + "</a>")
+}
+
+// acak
+random(arrayacak)
+for (nama in arrayacak) {
+    arrayacak[nama] = arrayacak[nama].split(", ")
 }
 acak = function(x) {
-    document.writeln("<a href='" + blog[x][1] + "' class='list-group-item'>" + blog[x][0] + "</a>")
+    document.writeln("<a href='" + arrayacak[x][1] + "' class='list-group-item'>" + arrayacak[x][0] + "</a>")
 }
--- blog/js/data.js
+++ blog/js/data.js
@@ -1,4 +1,6 @@
 blog = [
+    "Besok Harus Lebih Baik!, harus.html",
+    "Karena Hidup Bukan Sekedar Menulis, tulisan.html",
     "Penghujung Ramadan yang Amat Berkesan, penghujung.html",
     "Mengapa Aku Memilih Statis Page Buat Blogging?, mengapa.html",
     "Hello World, hello.html"
