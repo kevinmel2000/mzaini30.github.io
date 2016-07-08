aff1727461fb4933b3a714a4df58cfab48ced4e6
mzaini30 <muhzaini30@gmail.com>
2016-07-09 00:14:11 +0800

    update lagi nih tengah malam

--- blog/js/data.js
+++ blog/js/data.js
@@ -1,4 +1,6 @@
 blog = [
+    "Kau Pasti Bisa Melakukan Apapun, bisa.html",
+    "Hari Baru!, newday.html",
     "Besok Harus Lebih Baik!, harus.html",
     "Karena Hidup Bukan Sekedar Menulis, tulisan.html",
     "Penghujung Ramadan yang Amat Berkesan, penghujung.html",
