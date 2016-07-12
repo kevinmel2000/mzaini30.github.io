acf74a2c8afff3d5e4aae2e419f9e6a73bf3291b
mzaini30 <muhzaini30@gmail.com>
2016-07-13 06:17:51 +0800

    baru

--- blog/js/data.js
+++ blog/js/data.js
@@ -1,4 +1,5 @@
 blog = [
+    "Karena Hidup adalah Kumpulan Cerita, cerita.html",
     "Pentingnya Keterampilan, terampil.html",
     "Mencoba Fokus, fokus.html",
     "Menebar Kemanfaatan, manfaat.html",
