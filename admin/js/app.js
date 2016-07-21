45eac828b4d11a867a8e1d57fc31e7e69f961d36
mzaini30 <muhzaini30@gmail.com>
2016-07-21 11:20:06 +0800

    hello world

---
+++ admin/js/app.js
@@ -0,0 +1,36 @@
+// deklarasi fungsi
+random = function(array) {
+    array.sort(function() {
+        return Math.random() - .5
+    })
+}
+
+// duplikat array
+arraybaru = blog.slice(0)
+arrayacak = blog.slice(0)
+
+// baru
+for (nama in arraybaru) {
+    arraybaru[nama] = arraybaru[nama].split(", ")
+}
+baru = function(x) {
+    document.writeln("<a href='" + arraybaru[x][1] + "' class='list-group-item'>" + arraybaru[x][0] + "</a>")
+}
+
+// acak - diacak 10 kali biar bener-bener ngacak
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+random(arrayacak)
+for (nama in arrayacak) {
+    arrayacak[nama] = arrayacak[nama].split(", ")
+}
+acak = function(x) {
+    document.writeln("<a href='" + arrayacak[x][1] + "' class='list-group-item'>" + arrayacak[x][0] + "</a>")
+}
---
+++ admin/md/index.md
@@ -0,0 +1,7 @@
+# Prolog
+
+_Minggu, 17 Juli 2016_
+
+> Jangan Ikuti Mimpimu, Kejarlah Mimpimu
+
+> <footer>Richard Dumb #82</footer>
