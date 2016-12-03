55da0edaa4d52fa4d5946b74dad92266ab9451ea
Muhammad Zaini <muhzaini30@gmail.com>
2016-12-03 14:06:25 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 $(function() {
-    $masonry = $(".masonry").imagesLoaded(function() {
-        $masonry.masonry({
-            itemSelector: ".masonry-item"
-        })
-    })
+	$masonry = $(".masonry").imagesLoaded(function() {
+		$masonry.masonry({
+			itemSelector: ".masonry-item"
+		})
+	})
+	lagu = function() {
+		$(".soundcloud").height($(".soundcloud").width())
+	}
+	lagu()
+	$(window).resize(function() {
+		lagu()
+	})
 })
