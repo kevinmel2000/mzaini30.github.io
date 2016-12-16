8edf4492846188a6a5ee2073c93bfbf04bc37267
Muhammad Zaini <muhzaini30@gmail.com>
2016-12-16 23:31:29 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 $(function() {
+	anon = function() {
+		$(".header").attr({
+			"src": "gambar/header-anon.jpg"
+		})
+		$(".masonry-item img").attr({
+			"src": "gambar/logo-anon2.jpg"
+		})
+		$(".footer").removeClass("w3-deep-purple").addClass("w3-black")
+		$(".footer p").html("We are Anonymous. We are Legion. We do not Forgive. We do not Forget. Expect Us")
+		$("html, body").animate({
+			scrollTop: 0
+		}, 800)
+	}
 	$masonry = $(".masonry").imagesLoaded(function() {
 		$masonry.masonry({
 			itemSelector: ".masonry-item"
